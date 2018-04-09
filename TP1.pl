cbcf8ef32890742d9cf8d8523dbeb55b5c707f13
JhosuaArias <jhosuarqui@hotmail.com>
2018-04-08 18:39:11 -0600

    Maquina encriptadora

--- TP1.pl
+++ TP1.pl
@@ -15,7 +15,7 @@ agregar(E,[C|Cr],F):-
 	union(M,[P],F).
    
    
-% cartesiano/2(+A,+B,-C) Devuelve una lista a la que se le aplicó la operación cartesiano
+%cartesiano/2(+A,+B,-C) Devuelve una lista a la que se le aplicó la operación cartesiano
 cartesiano(X,_,_) :- not(is_list(X)), write('error: al menos uno de los argumentos no es un conjunto'), !, fail.
 cartesiano(_,X,_) :- not(is_list(X)), write('error: al menos uno de los argumentos no es un conjunto'), !, fail.
 cartesiano([],_,[]) :- !.
@@ -25,3 +25,44 @@ cartesiano([X|Xr],[Y|Yr],C):-
 	cartesiano([X],Yr,R1),
 	append(R1,R2,R3),
 	append([[X,Y]],R3,C).
+	
+%encripta/5(+He,+Ae,+As,-Hs,-Ef) He: hilera a encriptar, Ae: alfabeto de entrada, As: alfabeto de salida
+%Ef: estado final de la maquina formado por un par [ae,as], donde ae y as son simbolos del alfabeto de 
+%entrada y salida en lo que quedo la maquina luego de encriptar He. Hs: hilera encriptada.
+encripta(He,Ae,As,Hs,Ef):- 
+	not(is_list(He)),
+	not(is_list(Ae)),
+	not(is_list(As)),
+	write('error: alguno de los argumentos no es válido'),!, fail.
+encripta(He,Ae,As,Hs,Ef):-
+	crea_alfabeto_entrada(Ae),
+	crea_alfabeto_salida(As).
+%decripta/5(+Hs,+Ae,+As,+Ef,-He) Hs: hilera encriptada, Ae: alfabeto de entrada, As: alfabeto de salida,
+%Ef: estado final de la maquina formado por un par [ae,as], donde ae y as son simbolos del alfabeto de 
+%entrada y salida en lo que quedo la maquina luego de encriptar He. He: hilera desencriptada.
+decripta(Hs,Ae,As,Ef,He):-
+	not(is_list(Hs)),
+	not(is_list(Ae)),
+	not(is_list(As)),
+	not(is_list(Ef)),
+	write('error: alguno de los argumentos no es válido'),!, fail.
+decripta(Hs,Ae,As,Ef,He):-
+	crea_alfabeto_entrada(Ae),
+	crea_alfabeto_salida(As).
+
+%crea_alfabeto_entrada/1(+Ae) crea un hecho para que Ae sea un alfabeto de entrada
+crea_alfabeto_entrada(Ae):- assert(alfabetoE(Ae)).
+%crea_alfabeto_salida/1(As) crea un hecho para que As sea un alfabeto de salida
+crea_alfabeto_salida(As):- assert(alfabetoS(As)).
+
+%rota_alfabeto_entrada/1(L) rota el alfabeto de entrada un diente
+rota_alfabeto_entrada(L):- 
+	retract(alfabetoE([Cabeza|Resto])),
+	append(Resto,[Cabeza],L),
+	assert(alfabetoE(L)).
+%rota_alfabeto_salida/1(L) rota el alfabeto de salida un diente
+rota_alfabeto_salida(L):-	
+	retract(alfabetoS([Cabeza|Resto])),
+	append(Resto,[Cabeza],L),
+	assert(alfabetoS(L)).
+
